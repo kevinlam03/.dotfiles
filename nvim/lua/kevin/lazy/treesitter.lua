5def9437a418f3f540f82b10dd4fdf056de12450
Kevin Lam <lamk5977@gmail.com>
2024-05-14 02:09:15 -0400

    Fixed bug in setup of treesitter-context.

--- nvim/lua/kevin/lazy/treesitter.lua
+++ nvim/lua/kevin/lazy/treesitter.lua
@@ -1,33 +1,39 @@
 -- Treesitter
 return {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-treesitter/nvim-treesitter-context",
-    build = ":TSUpdate",
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            ensure_installed = {
-                "typescript",
-                "javascript",
-                "vim",
-                "vimdoc",
-                "query",
-                "python"
-            },
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true,
-            },
-        }
-        require('nvim-treesitter-context').setup {
-            mode = 'topline',
-        }
-    end
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require('treesitter-context').setup {
+                mode = 'topline',
+            }
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = {
+                    "typescript",
+                    "javascript",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "python"
+                },
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = false,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
+            }
+        end
+    }
 }
