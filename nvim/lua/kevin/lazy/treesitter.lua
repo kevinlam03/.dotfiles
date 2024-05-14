165f81cd88f85d06c4afa77f6c3152d6e1d6e70b
Kevin Lam <lamk5977@gmail.com>
2024-05-14 02:33:45 -0400

    Toggle context on and off.

--- nvim/lua/kevin/lazy/treesitter.lua
+++ nvim/lua/kevin/lazy/treesitter.lua
@@ -3,9 +3,12 @@ return {
     {
         "nvim-treesitter/nvim-treesitter-context",
         config = function()
-            require('treesitter-context').setup {
+            local context = require('treesitter-context')
+            context.setup {
+                enable = false,
                 mode = 'topline',
             }
+            vim.keymap.set('n', '[oc', '<cmd>TSContextToggle<cr>')
         end
     },
     {
